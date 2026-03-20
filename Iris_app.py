f3415e7bf8f174e626f8994e81e5009475129fde
devanshg-20 <gdevansh755@gmail.com>
2026-03-20 16:56:25 +0530

    Iris_app.py

--- Iris_app.py
+++ Iris_app.py
@@ -2,11 +2,10 @@ import streamlit as st
 import joblib
 import numpy as np
 
-# Load models
+# Load model
 nb_model = joblib.load("naive_bayes_model.pkl")
 
 st.title("🌸 Iris Flower Prediction App")
-
 
 st.write("Enter flower measurements:")
 
@@ -16,9 +15,23 @@ sepal_width = st.number_input("Sepal Width")
 petal_length = st.number_input("Petal Length")
 petal_width = st.number_input("Petal Width")
 
+# Prediction button
 if st.button("Predict"):
     input_data = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
 
     prediction = nb_model.predict(input_data)
 
-    st.success(f"Predicted Class: {prediction[0]}")
+    predicted_class = prediction[0]
+
+    st.success(f"🌸 Predicted Class: {predicted_class}")
+
+    # Map prediction to images
+    image_map = {
+        "Iris-setosa": "Iris_images/setosa.jpg",
+        "Iris-versicolor": "Iris_images/versicolor.jpg",
+        "Iris-virginica": "Iris_images/virginica.jpg"
+    }
+
+    # Show image
+    if predicted_class in image_map:
+        st.image(image_map[predicted_class], caption=predicted_class, use_column_width=True)
